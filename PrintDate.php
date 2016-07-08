62d000b908751d3df915d31e7a5eda3c58c86148
osovskaya <tanya.osovskaya@mail.ru>
2016-07-08 21:35:39 +0300

    002: print function renamed

--- PrintDate.php
+++ PrintDate.php
@@ -1,14 +1,12 @@
 <?php
 
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-
 class PrintDate 
 {
-	public function printDate()
+	public function printCurrentDate()
 	{
 		echo date('F j, Y'); // March 10, 2001
 	}
 }
 
 $date = new PrintDate;
+$date->printCurrentDate();
